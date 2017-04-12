8ea8588810a3b8f9e287872cd555756bc811841c
yulgurrea <yulgurz@gmail.com>
2017-04-12 23:03:55 +0800

    added import flask in app.py

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify
+import json, os, sys
+import flask
+from config import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def hellow_world():
+    return "hellow world!"
+
+@app.route("/admin/<string:email>", methods=['POST'])
+def new_admin():
+	jsn = json.loads(request.data)
+
+	res = spcall("new_admin", (
+		jsn['email'],
+		jsn['password']), True)
+
+	if 'Error' in res[0][0]:
+		return jsonify({'status': 'Error', 'message': res[0][0]})
+
+	return jsonify({'status': 'ok', 'message': res[0][0]})
+
+@app.after_request
+def add_cors(resp):
+    resp.headers['Access-Control-Allow-Origin'] = flask.request.headers.get('Origin', '*')
+    resp.headers['Access-Control-Allow-Credentials'] = True
+    resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET, PUT, DELETE'
+    resp.headers['Access-Control-Allow-Headers'] = flask.request.headers.get('Access-Control-Request-Headers',
+                                                                             'Authorization')
+    # set low for debugging
+
+    if app.debug:
+        resp.headers["Access-Control-Max-Age"] = '1'
+    return resp
+
+
+if __name__ == '__main__':
+    app.secret_key = 'B1Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+    # app.run(host='localhost', debug=True)
+    app.run(host='0.0.0.0', debug=True)
