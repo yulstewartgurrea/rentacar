b0671d5d2808eb13d851b9784b4b1e6e42efd284
yulgurrea <yulgurz@gmail.com>
2017-04-27 16:17:07 +0800

    updated routes.js

--- app/static/js/routes.js
+++ app/static/js/routes.js
@@ -1,8 +1,31 @@
 $(document).ready(function(){
 	$("#showmain").click(function(){
 		$("#main").show();
+		$("#userprofile").hide();
+		$("#addcar").hide();
 		$("#tableforcars").hide();
 		
+	});
+
+	$("#showuserprofile").click(function(){
+		$("#main").hide();
+		$("#userprofile").show();
+		$("#addcar").hide();
+		$("#tableforcars").hide();
+	});
+
+	$("#showaddcar").click(function(){
+		$("#main").hide();
+		$("#userprofile").hide();
+		$("#addcar").show();
+		$("#tableforcars").hide();
+	});
+
+	$("#showtableforcars").click(function(){
+		$("#main").hide();
+		$("#userprofile").hide();
+		$("#addcar").hide();
+		$("#tableforcars").show();
 	});
 
 });
