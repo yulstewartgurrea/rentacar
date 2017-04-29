57cc5870937d4b73203f07691319ebcff0bf77ad
yulgurrea <yulgurz@gmail.com>
2017-04-29 23:57:10 +0800

    added showshophomepage in routes, added shoptemplate, added customers shop page

--- app/static/js/routes.js
+++ app/static/js/routes.js
@@ -47,3 +47,18 @@ function showaddownerpage() {
 	$("#addownerpage").show();
 	$("#updatecarpage").hide();
 }
+
+function showshophomepage() {
+	$("#homepage").show();
+	$("#shoppage").hide();
+	$("#shoppage2").hide();
+	$("#shoppage3").hide();
+	$("#shoppage4").hide();
+	$("#cardetailspage").hide();
+	$("#cardetailspage2").hide();
+	$("#cartpage").hide();
+	$("#cartpage2").hide();
+	$("#checkoutpage").hide();
+	$("#checkoutpage2").hide();
+	$("#checkoutpage3").hide();
+}	
